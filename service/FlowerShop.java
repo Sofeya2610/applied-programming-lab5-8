b52a0c47d0c1a40492c293760f55aa78c8b22540
Sofeya2610 <sofiia.ostrovska.oi.2024@lpnu.ua>
2025-11-14 23:11:20 +0200

    Add files via upload
    add main function

---
+++ service/FlowerShop.java
@@ -0,0 +1,63 @@
+package service;
+
+public class FlowerShop {
+    // каталог
+    public FlowerShop() {
+        // Тут буде завантаження каталогу з файлу, дані введені користувачем будуть зберігатись
+        System.out.println("Вітаємо в магазині. Каталог поки порожній!");
+    }
+
+    // перегляд каталогу
+    public void viewCatalog() {
+        System.out.println("Виконується метод viewCatalog()");
+        // дальше буде логіка
+    }
+
+    // додати до каталогу
+    public void addFlowerToCatalog() {
+        System.out.println("Виконується метод addFlowerToCatalog()");
+        // дальше буде логіка
+    }
+
+    //Створення букету
+    public void createBouquet() {
+        System.out.println("Виконується створення нового букету...");
+        // дальше буде логіка
+    }
+
+    //Перегляд букетів
+    public void viewAllBouquets() {
+        System.out.println("Виконується перегляд всіх букетів...");
+        // дальше буде логіка
+    }
+
+    //Редагування букетів
+    public void editBouquet() {
+        System.out.println("Виконується редагування букету");
+        // дальше буде логіка
+    }
+
+    //Сортування букетів
+    public void sortBouquet() {
+        System.out.println("Виконується сортування квітів у букеті");
+        // дальше буде логіка
+    }
+
+    //Пошук квітки за стеблом
+    public void findFlowerInBouquet() {
+        System.out.println("Виконується пошук квітки за стеблом");
+        // дальше буде логіка
+    }
+
+    //збереження каталогу до файлу
+    public void saveCatalogToFile() {
+        System.out.println("Виконується збереження у файл");
+        // дальше буде логіка
+    }
+
+    // Видалення квітки з каталогу
+    public void deleteFlowerFromCatalog() {
+        System.out.println("Виконується видалення квітки з каталогу");
+        // дальше буде логіка
+    }
+}
